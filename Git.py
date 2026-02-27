13148fe33cec5082f3a5718a605cc190b30015d8
Trinity_JaMocha <trinityblush@gmail.com>
2026-02-27 01:47:34 +0000

    These are GIT BASICS

---
+++ Git.py
@@ -0,0 +1,28 @@
+# UPLOAD FILES  = clicking into repository and then clicking the Add File button, near the
+ # green code button
+
+# READ ME = describes what the repository is about
+
+# CODING LOCALLY
+# GitHub Desktop = application for source control
+
+# GIT 
+# To add your name and email address to the Git global configuration
+git config --global user.name 'Trinity_JaMocha'  # added to Terminal
+
+# GIT IS LIKE GOOGLE DOC # SOME EXAMPLES BELOW
+git innit # initializes a new git repository / like creating a google doc
+
+# GIT ADD
+git add # hold file b/c I have to take a screenshot and have tea to spill about file
+# like saving a google doc after writing, so it will be tracked
+
+# GIT COMMIT
+git commit # save changes of project for longterm and store them personally
+# like naming current version of google docs, so I can go back later
+
+# GIT STATUS
+git status # show current state of working directory and staging area, whats going on in my
+# folder / like seeing document status in Google Docs
+
+# GIT RESET HEAD
